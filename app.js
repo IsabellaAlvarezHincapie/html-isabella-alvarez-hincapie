eb40d014c047b0c43e6b1a1e4779cef025eba04d
IsabellaAlvarezHincapie <123569257+IsabellaAlvarezHincapie@users.noreply.github.com>
2023-02-06 12:06:34 -0500

    estilo de cuadricula con css e imagenes con js

---
+++ app.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    { name: 'aeropress'
+    img: 'img/aeropress.jpg'
+  },
+  { name: 'chemex'
+    img: 'img/chemex.jpg'
+  },
+  { name: 'moka'
+  img: 'img/moka.jpg'
+  },
+  { name: 'sifon'
+  img: 'img/sifon.jpg'
+  },
+  { name: 'ibrik'
+  img: 'img/ibrik.jpg'
+  },
+  { name: 'hario'
+  img: 'img/hario.jpg'
+  },
+  { name: 'aeropress'
+  img: 'img/aeropress.jpg'
+},
+{ name: 'chemex'
+  img: 'img/chemex.jpg'
+},
+{ name: 'moka'
+img: 'img/moka.jpg'
+},
+{ name: 'sifon'
+img: 'img/sifon.jpg'
+},
+{ name: 'ibrik'
+img: 'img/ibrik.jpg'
+},
+{ name: 'hario'
+img: 'img/hario.jpg'
+},
+  
+  
+   
+  ]
+});
