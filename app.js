6a20d28769010416ab743016305515da782ea2bf
IsabellaAlvarezHincapie <123569257+IsabellaAlvarezHincapie@users.noreply.github.com>
2023-02-13 11:56:30 -0500

    constantes y funcion for

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cardsAdj = [
-    { name: 'aeropress'
-    img: 'img/aeropress.jpg'
-  },
-  { name: 'chemex'
-    img: 'img/chemex.jpg'
-  },
-  { name: 'moka'
-  img: 'img/moka.jpg'
-  },
-  { name: 'sifon'
-  img: 'img/sifon.jpg'
-  },
-  { name: 'ibrik'
-  img: 'img/ibrik.jpg'
-  },
-  { name: 'hario'
-  img: 'img/hario.jpg'
-  },
-  { name: 'aeropress'
-  img: 'img/aeropress.jpg'
-},
-{ name: 'chemex'
-  img: 'img/chemex.jpg'
-},
-{ name: 'moka'
-img: 'img/moka.jpg'
-},
-{ name: 'sifon'
-img: 'img/sifon.jpg'
-},
-{ name: 'ibrik'
-img: 'img/ibrik.jpg'
-},
-{ name: 'hario'
-img: 'img/hario.jpg'
-},
-  
-  
-   
-  ]
+    { name: "aeropress", img: "img/aeropress.jpg" },
+    { name: "chemex", img: "img/chemex.jpg" },
+    { name: "moka", img: "img/moka.jpg" },
+    { name: "sifon", img: "img/sifon.jpg" },
+    { name: "ibrik", img: "img/ibrik.jpg" },
+    { name: "hario", img: "img/hario.jpg" },
+    { name: "aeropress", img: "img/aeropress.jpg" },
+    { name: "chemex", img: "img/chemex.jpg" },
+    { name: "moka", img: "img/moka.jpg" },
+    { name: "sifon", img: "img/sifon.jpg" },
+    { name: "ibrik", img: "img/ibrik.jpg" },
+    { name: "hario", img: "img/hario.jpg" }
+  ];
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  function crearTablero() {
+    for (let i = 0; i < cardsAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("clisck", voltearCarta);
+
+      cuadricula.appendChild(carta);
+    }
+  }
 });
